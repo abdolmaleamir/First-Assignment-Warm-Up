acfd0ed453c5938f995adc2a02b5b43a0556d38d
amirabdol <abdolmaleamir@gmail.com>
2024-02-26 12:24:36 +0330

    pasted all the changes from the old proj

--- src/main/java/Exercises1.java
+++ src/main/java/Exercises1.java
@@ -3,8 +3,11 @@ public class Exercises1 {
     implement a function that returns factorial of given n
      */
     public long factorial(int n) {
-        // TODO
-        return 0;
+        long x = 1;
+        for (int i = 2; i <= n; i++) {
+            x *= i;
+        }
+        return x;
     }
 
 
@@ -13,8 +16,18 @@ public class Exercises1 {
     the series -> 1, 1, 2, 3, 5, 8, ...
     */
     public long fibonacci(int n) {
-        // TODO
-        return 0;
+        long a = 1;
+        long b = 1;
+        long c = 0;
+        if ( n == 1 || n == 2)
+            return 1;
+        for (int i = 3; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return c;
     }
 
 
@@ -30,10 +43,17 @@ public class Exercises1 {
 
      */
     public char[][] generateTriangle(int rows) {
-        // TODO
-        return null;
-    }
+        char[][] triangle = new char[rows][];
 
+        for (int i = 0; i < rows; i++) {
+            triangle[i] = new char[i + 1];
+            for (int j = 0; j < i + 1; j++) {
+                triangle[i][j] = '*';
+            }
+        }
+        return triangle;
+
+    }
 
     public static void main(String[] args) {
         // test your code here!
